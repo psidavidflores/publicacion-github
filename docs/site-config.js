030f737928e9ee09ec81ad9403efa0ff10fdcc8d
psidavidflores <psidavidflores@gmail.com>
2026-09-09 14:24:51 -0500

    Conectar descargas con los releases de GitHub

--- docs/site-config.js
+++ docs/site-config.js
@@ -1,8 +1,8 @@
 window.SITE_CONFIG = Object.freeze({
   title: "Kit de Terapia del Lenguaje",
   subtitle: "Biblioteca digital de recursos profesionales",
-  releaseBaseUrl: "",
-  releasePageUrl: "",
+  releaseBaseUrl: "https://github.com/psidavidflores/publicacion-github/releases/latest/download",
+  releasePageUrl: "https://github.com/psidavidflores/publicacion-github/releases/latest",
   versionLabel: "versión 1.0",
   brand: {
     turquoise: "#39e6d0",
